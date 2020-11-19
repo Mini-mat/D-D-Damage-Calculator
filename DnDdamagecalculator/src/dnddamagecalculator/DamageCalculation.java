53fc46b30898e9c6d1bfd47e5f5686c3f8cbc208
Mini-mat <66658615+Mini-mat@users.noreply.github.com>
2020-11-19 05:03:18 -0300

    Advantage switch statement added
    Added advantage throught a switch statement

--- DnDdamagecalculator/src/dnddamagecalculator/DamageCalculation.java
+++ DnDdamagecalculator/src/dnddamagecalculator/DamageCalculation.java
@@ -23,31 +23,97 @@ public class DamageCalculation {
     
     //Damage
     private int damageDice;
+    private int damageBonus;
     
     
-    //Damage calculation code
-    public String calcDamage(){
+    //damage calculation code switch statement version
+    public String calcDamageSwitch(){
         int ac;
         float avgDamage = 0;
         float avgCalculator = 0;
-        for (ac = 10;ac <= 32; ac++){     
-            for (int i = 1; i <=2000000; i++){
-                int natScore = die.getD20();
-                int attackScore = natScore + this.getAttackBonus();
-                if (natScore >= this.getCritical()){
-                    avgCalculator += this.getDamageDice();
-                    avgCalculator += this.getDamageDice();
-                } else if (attackScore >= ac && natScore != 1) {
-                    avgCalculator += this.getDamageDice();
-                }
-            avgDamage = round(avgCalculator / 2000000);
-            }
+        String returnString = "";
+        switch(this.getAdvantage()) {
+            case "Normal":
+                    for (ac = 10;ac <= 32; ac++){     
+                        for (int i = 1; i <=2000000; i++){
+                            int natScore = die.getD20();
+                            int attackScore = natScore + this.getAttackBonus();
+                                if (natScore >= this.getCritical()){
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                } else if (attackScore >= ac && natScore != 1) {
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                }
+                        avgDamage = round(avgCalculator / 2000000);
+                        }
+                    returnString += "Average damage for AC " + ac + ": " + avgDamage + System.lineSeparator();
+                    }
+                    break;
+                    
+            case "Advantage":
+                for (ac = 10;ac <= 32; ac++){     
+                        for (int i = 1; i <=2000000; i++){
+                            int natScore = Math.max(die.getD20(), die.getD20());
+                            int attackScore = natScore + this.getAttackBonus();
+                                if (natScore >= this.getCritical()){
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                } else if (attackScore >= ac && natScore != 1) {
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                }
+                        avgDamage = round(avgCalculator / 2000000);
+                        }
+                        returnString += "Average damage for AC " + ac + ": " + avgDamage + System.lineSeparator();
+                    }
+                    break;
+            
+            case "Disavantage":
+                for (ac = 10;ac <= 32; ac++){     
+                        for (int i = 1; i <=2000000; i++){
+                            int natScore = Math.min(die.getD20(), die.getD20());
+                            int attackScore = natScore + this.getAttackBonus();
+                                if (natScore >= this.getCritical()){
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                } else if (attackScore >= ac && natScore != 1) {
+                                    avgCalculator += this.getDamageDice();
+                                    avgCalculator += this.getDamageBonus();
+                                }
+                        avgDamage = round(avgCalculator / 2000000);
+                        }
+                        returnString += "Average damage for AC " + ac + ": " + avgDamage + System.lineSeparator();
+                    }
+                    break;
+                    
+            case "Elven Accuracy":
+                for (ac = 10;ac <= 32; ac++){     
+                            for (int i = 1; i <=2000000; i++){
+                                int natScore = Math.max(die.getD20(), Math.max(die.getD20(), die.getD20()));
+                                int attackScore = natScore + this.getAttackBonus();
+                                    if (natScore >= this.getCritical()){
+                                        avgCalculator += this.getDamageDice();
+                                        avgCalculator += this.getDamageDice();
+                                        avgCalculator += this.getDamageBonus();
+                                    } else if (attackScore >= ac && natScore != 1) {
+                                        avgCalculator += this.getDamageDice();
+                                        avgCalculator += this.getDamageBonus();
+                                    }
+                            avgDamage = round(avgCalculator / 2000000);
+                            }
+                            returnString += "Average damage for AC " + ac + ": " + avgDamage + System.lineSeparator();
+                        }
+                        break;
+            
         }
-        String returnVariable = "Average damage for AC " + ac + ":" + avgDamage;
-        return returnVariable;
+        return returnString;
     }
 
-    public int getAttackBonus() {
+    public int getAttackBonus(){
         return attackBonus;
     }
 
@@ -62,6 +128,31 @@ public class DamageCalculation {
     public int getDamageDice() {
         return damageDice;
     }
+
+    public int getDamageBonus() {
+        return damageBonus;
+    }
+
+    public void setAttackBonus(int attackBonus) {
+        this.attackBonus = attackBonus;
+    }
+
+    public void setCritical(int critical) {
+        this.critical = critical;
+    }
+
+    public void setAdvantage(String advantage) {
+        this.advantage = advantage;
+    }
+
+    public void setDamageDice(int damageDice) {
+        this.damageDice = damageDice;
+    }
+
+    public void setDamageBonus(int damageBonus) {
+        this.damageBonus = damageBonus;
+    }
+    
     
     
 }
